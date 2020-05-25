ec341e859ef45f17287d7b3e2ae883543512ac9c
Mark Ledger <mark_ledger@outlook.com>
2020-05-25 22:03:50 +0100

    'opo'

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
-sudo chmod -R 0777 /var/www/html/storage
-sudo chmod -R 0777 /var/www/html/bootstrap/cache
+#sudo chmod -R 0777 /var/www/html/storage
+#sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
-cd /var/www/html
+#cd /var/www/html
 #
 # Run composer
-sudo /usr/bin/composer.phar install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
+#sudo /usr/bin/composer.phar install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
 #
 # Run artisan commands
-php /var/www/html/artisan migrate
+#php /var/www/html/artisan migrate
